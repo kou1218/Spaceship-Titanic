1563af55551dac69ffca17d3e5ee2a99fc053741
saeki <hikaru-1st.toshiba@docomo.ne.jp>
2024-04-30 15:04:50 +0900

    add:csvファイルの出力

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -81,6 +81,7 @@ class ExpBase:
         return model, end
     
     def run(self):
+        self.train.to_csv("train.csv")
         skf = StratifiedKFold(n_splits=self.n_splits, shuffle=True, random_state=self.seed)
         y_test_pred_all = []
         score_all = 0
@@ -155,7 +156,7 @@ class ExpOptuna(ExpBase):
         x, y = self.get_x_y(self.train)
         print(self.get_model_config(1, x, y))
 
-    def get_model_config(self, i_fold, x, y, val_data):
+    def get_model_config(self, i_fold, x, y):
         op = OptimParam(
             self.model_name,
             default_config=self.model_config,
